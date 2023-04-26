0c8affa697f2578a23c46f82ff0564bb1a6e896f
Minjun <mj061101k@naver.com>
2023-04-26 18:42:07 +0900

    timer

--- lib/main.dart
+++ lib/main.dart
@@ -14,23 +14,63 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  // int totalTime = 60;
   int times = 1 * 60; // 1min
   late Timer timer;
+  String timeView = '0:00:00';
+  bool isRunning = false;
+
   void timeStart() {
-    // 1초마다 1씩 내려감.. 일정간격마다 수행
-    Timer timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+    if (isRunning) {
+      // 돌고 있는가? => 시간을 멈춤, 상태 변경
+      timeStop();
       setState(() {
-        times--;
+        isRunning = !isRunning;
       });
-    });
+    } else {
+      //안돌고 있음 => 돌아감, 변경
+
+      // 1초마다 1씩 내려감.. 일정간격마다 수행
+      // periodic = 주기
+      setState(() {
+        isRunning = !isRunning;
+      });
+      timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+        setState(() {
+          timeView = Duration(seconds: times).toString().split('.').first;
+          times--;
+          if (times < 0) {
+            timeStop();
+          }
+        });
+      });
+    }
   }
 
   void timeStop() {
     timer.cancel();
   }
 
+  void timeReset() {
+    setState(() {
+      times = 60;
+      isRunning = false;
+      timeStop();
+      timeView = Duration(seconds: times).toString().split('.').first;
+    });
+  }
+
+  void addTime(int sec) {
+    times = times + sec;
+    times = times < 0 ? 0 : times;
+    setState(() {
+      timeView = Duration(seconds: times).toString().split('.').first;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    timeView = Duration(seconds: times).toString().split('.').first;
     return MaterialApp(
       home: Scaffold(
         body: Column(children: [
@@ -49,14 +89,29 @@ class _MyAppState extends State<MyApp> {
             ),
           ),
           Flexible(
-            flex: 3,
+              flex: 1,
+              child: SizedBox(
+                width: double.infinity,
+                height: double.infinity,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    timeButton(sec: 60, color: Colors.amber),
+                    timeButton(sec: 30, color: Colors.red),
+                    timeButton(sec: -30, color: Colors.lightBlue),
+                    timeButton(sec: -60, color: Colors.purpleAccent),
+                  ],
+                ),
+              )),
+          Flexible(
+            flex: 2,
             child: Container(
               width: double.infinity,
               height: double.infinity,
               color: Colors.red,
               child: Center(
                 child: Text(
-                  '$times',
+                  timeView,
                   style: const TextStyle(
                       color: Color.fromRGBO(255, 255, 255, 1), fontSize: 50),
                 ),
@@ -73,19 +128,38 @@ class _MyAppState extends State<MyApp> {
                     child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
+                    if (isRunning)
+                      IconButton(
+                          iconSize: 50,
+                          onPressed: timeStart,
+                          icon: const Icon(Icons.pause_circle_rounded))
+                    else
+                      IconButton(
+                          iconSize: 50,
+                          onPressed: timeStart,
+                          icon: const Icon(Icons.play_circle_rounded)),
                     IconButton(
                         iconSize: 50,
-                        onPressed: timeStart,
-                        icon: const Icon(Icons.play_circle_rounded)),
-                    const IconButton(
-                        iconSize: 50,
-                        onPressed: null,
-                        icon: Icon(Icons.pause_circle_rounded))
+                        onPressed: timeReset,
+                        icon: const Icon(Icons.restore)),
                   ],
                 ))),
           ),
         ]),
-        backgroundColor: Colors.black,
+      ),
+    );
+  }
+
+  GestureDetector timeButton({required int sec, required Color color}) {
+    return GestureDetector(
+      onTap: () => addTime(sec),
+      child: Container(
+        width: 100,
+        height: 100,
+        decoration: BoxDecoration(shape: BoxShape.circle, color: color),
+        child: Center(
+          child: Text('$sec'),
+        ),
       ),
     );
   }
